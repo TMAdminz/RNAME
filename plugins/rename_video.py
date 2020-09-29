e38f2a65b8a09d5581f84a503b57bf40edec93be
Monk Tea <71178046+MonkTeam@users.noreply.github.com>
2020-09-29 14:10:48 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -121,7 +121,7 @@ async def rename_video(bot, update):
                 video=new_file_name,
                 duration=duration,
                 thumb=thumb_image_path,
-                caption=description,
+                caption=f"<b>{file_name} \n\nShare and Support\n\n@SonyPictures_Tamil</b>",
                 # reply_markup=reply_markup,
                 reply_to_message_id=update.reply_to_message.message_id,
                 progress=progress_for_pyrogram,
